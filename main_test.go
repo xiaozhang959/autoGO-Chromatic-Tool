3b1f8876fc836934a0aa5b0668e709479c732bc5
xiaozhang959 <1273993501@qq.com>
2026-06-02 18:30:12 +0800

    test: cover official image api param formats

--- main_test.go
+++ main_test.go
@@ -2,35 +2,77 @@ package main
 
 import "testing"
 
-func TestBuildImagesAPICodeFindMultiColorsColorExport(t *testing.T) {
+func withColorPointsForTest(t *testing.T, points []ColorPoint) {
+	t.Helper()
+
 	oldPoints := colorPoints
 	oldRectCoordEntry := rectCoordEntry
-	defer func() {
+	t.Cleanup(func() {
 		colorPoints = oldPoints
 		rectCoordEntry = oldRectCoordEntry
-	}()
+	})
 
 	rectCoordEntry = nil
-	colorPoints = []ColorPoint{
+	colorPoints = points
+}
+
+func TestBuildImagesAPICodeColorExportUsesOfficialParamOrder(t *testing.T) {
+	withColorPointsForTest(t, []ColorPoint{
 		{Position: "10, 20", Color: "#081029", Offset: "202020", Selected: true},
 		{Position: "197, -1", Color: "#1C3A6D", Offset: "202020", Selected: true},
 		{Position: "42, 148", Color: "#0D1B3C", Offset: "202020", Selected: true},
+	})
+
+	tests := []struct {
+		name      string
+		function  string
+		wantColor string
+		wantParam string
+		wantCode  string
+	}{
+		{
+			name:      "FindColor",
+			function:  "FindColor",
+			wantColor: `{0,0,0,0,"081029-202020|1c3a6d-202020|0d1b3c-202020",0.9,0,0}`,
+			wantParam: `0, 0, 0, 0, "081029-202020|1c3a6d-202020|0d1b3c-202020", 0.9, 0, 0`,
+			wantCode:  `x, y := images.FindColor(0, 0, 0, 0, "081029-202020|1c3a6d-202020|0d1b3c-202020", 0.9, 0, 0)`,
+		},
+		{
+			name:      "FindMultiColors",
+			function:  "FindMultiColors",
+			wantColor: `{0,0,0,0,"081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020",0.9,0,0}`,
+			wantParam: `0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0`,
+			wantCode:  `x, y := images.FindMultiColors(0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0)`,
+		},
+		{
+			name:      "FindMultiColorsAll",
+			function:  "FindMultiColorsAll",
+			wantColor: `{0,0,0,0,"081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020",0.9,0,0}`,
+			wantParam: `0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0`,
+			wantCode:  `points := images.FindMultiColorsAll(0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0)`,
+		},
+		{
+			name:      "CmpColor",
+			function:  "CmpColor",
+			wantColor: `{10,20,"081029-202020|1c3a6d-202020|0d1b3c-202020",0.9,0}`,
+			wantParam: `10, 20, "081029-202020|1c3a6d-202020|0d1b3c-202020", 0.9, 0`,
+			wantCode:  `matched := images.CmpColor(10, 20, "081029-202020|1c3a6d-202020|0d1b3c-202020", 0.9, 0)`,
+		},
 	}
 
-	colorText, params, code := buildImagesAPICode("FindMultiColors", "0.9", "0: 从左到右，从上到下")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			colorText, params, code := buildImagesAPICode(tt.function, "0.9", "0: 从左到右，从上到下")
 
-	wantColorText := `{0,0,0,0,"081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020",0.9,0,0}`
-	if colorText != wantColorText {
-		t.Fatalf("color text mismatch:\nwant: %s\n got: %s", wantColorText, colorText)
-	}
-
-	wantParams := `0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0`
-	if params != wantParams {
-		t.Fatalf("params mismatch:\nwant: %s\n got: %s", wantParams, params)
-	}
-
-	wantCode := `x, y := images.FindMultiColors(0, 0, 0, 0, "081029-202020,187,-21,1c3a6d-202020,32,128,0d1b3c-202020", 0.9, 0, 0)`
-	if code != wantCode {
-		t.Fatalf("code mismatch:\nwant: %s\n got: %s", wantCode, code)
+			if colorText != tt.wantColor {
+				t.Fatalf("color text mismatch:\nwant: %s\n got: %s", tt.wantColor, colorText)
+			}
+			if params != tt.wantParam {
+				t.Fatalf("params mismatch:\nwant: %s\n got: %s", tt.wantParam, params)
+			}
+			if code != tt.wantCode {
+				t.Fatalf("code mismatch:\nwant: %s\n got: %s", tt.wantCode, code)
+			}
+		})
 	}
 }
